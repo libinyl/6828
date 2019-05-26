8a2cefe2ca91408e9e6d80cba631c0ff9ba4dfdc
libinyl <1102109100@qq.com>
2019-05-26 18:32:36 +0800

    enrich init.lab and readme.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# yl-6828
+## linux 常用命令
+
+查看电脑是多少位
+
+`getconf LONG_BIT`
+
+从本地上传到服务器
+
+`scp 源 宿`
+
+## qemu 常用命令
+
+```
+./configure --disable-kvm --disable-werror  --target-list="i386-softmmu x86_64-softmmu"
+```
+
+如果缺少`pkg-config`：
+
+```
+
+```
--- initlab.sh
+++ initlab.sh
@@ -1,13 +1,51 @@
 #!/usr/bin/env bash
-
+echo "test"
 workpath="${HOME}/6828"
 
 if [[ -d ${workpath} ]];then
-    echo "path ${workpath} already exists, exit."
-    exit 1
+    echo "t2"
+    read -p "path \"${workpath}\" already exists. To force init,press 'f'; 'e' or others to exit." f1
+    case $f1 in
+    'f')    rm -rf ${workpath}  break   ;;
+     * )    echo "exit."        exit 0  ;;
+    esac
+
 fi
 
-mkdir ${workpath}
-cd ${workpath}
-git clone https://gitee.com/libinyl/6.828-qemu.git qemu
-git clone https://pdos.csail.mit.edu/6.828/2018/jos.git lab
+
+mkdir   ${workpath}
+cd      ${workpath}
+
+read -p "where do you prefer to download the qemu code? press 'p' for personal; 'o' or others for offical:" f2
+case $f2 in
+    "p") qemupath="https://gitee.com/libinyl/6.828-qemu.git"    ;;
+     * ) qemupath="https://github.com/mit-pdos/6.828-qemu.git"  ;;
+esac
+
+labpath="https://pdos.csail.mit.edu/6.828/2018/jos.git"
+
+# clone qemu code.
+echo "start to clone qemu code..."
+git clone ${qemupath} qemu
+
+if [[ $? -ne 0 ]]; then
+    echo "clone qemu code failed.exit."
+    exit 0;
+else
+    echo "clone qemu code done."
+fi
+
+
+# clone lab code
+echo "start to clone lab code..."
+
+git clone ${labpath} lab
+if [[ $? != 0 ]]; then
+    echo "clone lab code failed.exit."
+    exit 0;
+else
+    echo "clone lab code done."
+fi
+
+
+echo "init all lab code done."
